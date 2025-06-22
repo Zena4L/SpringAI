6982c50fc8e73a7fcdf81c5eb6a4eeb2a8524829
clement.owireku-bogyah@amalitech.com <clement.owireku-bogyah@amalitech.com>
2025-06-22 19:35:24 +0000

    feat: advisor

--- src/main/java/com/clement/assistant/services/implementation/QuestionService.java
+++ src/main/java/com/clement/assistant/services/implementation/QuestionService.java
@@ -7,6 +7,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.metadata.ChatResponseMetadata;
 import org.springframework.ai.chat.metadata.Usage;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -19,21 +20,23 @@ import java.util.logging.Logger;
 public class QuestionService implements ChatService {
     private final ChatClient chatClient;
     private final RulesService rulesService;
+    private final VectorStore vectorStore;
     @Value("classpath:promptTemplates/systemPromptTemplate.st")
     Resource promptTemplate;
 
     private static final Logger logger = Logger.getLogger(QuestionService.class.getName());
 
-    public QuestionService(ChatClient.Builder chatClientBuilder, RulesService rulesService) {
+    public QuestionService(ChatClient.Builder chatClientBuilder, RulesService rulesService, VectorStore vectorStore) {
         this.chatClient = chatClientBuilder
             .build();
         this.rulesService = rulesService;
+        this.vectorStore = vectorStore;
     }
 
     @Override
     public Answer askQuestion(Question question) {
         String ideaRule = rulesService.stuffingRule(question.title());
-        var responseEntity =  chatClient.prompt()
+        var responseEntity = chatClient.prompt()
             .system(systemSpec ->
                 systemSpec.text(promptTemplate)
                     .param("title", question.title())
@@ -45,15 +48,15 @@ public class QuestionService implements ChatService {
 //        return new Answer(question.title(), answerText);
 
         ChatResponse response = responseEntity.response();
-        ChatResponseMetadata metadata =  response.getMetadata();
+        ChatResponseMetadata metadata = response.getMetadata();
 
         logUsage(metadata.getUsage());
 
-        return  responseEntity.entity();
+        return responseEntity.entity();
     }
 
     private void logUsage(Usage usage) {
-       logger.info("Usage details: " +
+        logger.info("Usage details: " +
             "Prompt tokens: " + usage.getPromptTokens() + ", " +
             "Completion tokens: " + usage.getCompletionTokens() + ", " +
             "Total tokens: " + usage.getTotalTokens()
--- src/main/java/com/clement/assistant/services/implementation/RulesService.java
+++ src/main/java/com/clement/assistant/services/implementation/RulesService.java
@@ -1,15 +1,26 @@
 package com.clement.assistant.services.implementation;
 
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.filter.FilterExpressionBuilder;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @Service
 public class RulesService {
     private static final Logger logger = Logger.getLogger(RulesService.class.getName());
+    private final VectorStore vectorStore;
+
+    public RulesService(VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
+    }
 
     public String stuffingRule(String input) {
         try {
@@ -24,5 +35,26 @@ public class RulesService {
             logger.severe("Error loading rule file: " + e.getMessage());
             return "Rule not found or error loading rule.";
         }
+    }
+
+    public String getRoleForRag(String gameName, String question) {
+        SearchRequest searchRequest = SearchRequest.builder()
+            .query(question)
+            .topK(6)
+            .similarityThreshold(0.5)
+            .filterExpression(new FilterExpressionBuilder().eq("gameName", normalizedGameTitle(gameName)).build())
+            .build();
+
+
+        List<Document> similarDocs = vectorStore.similaritySearch(searchRequest);
+        if (similarDocs.isEmpty()) {
+            return "No relevant information found for the question.";
+        }
+
+        return similarDocs.stream().map(Document::getText).collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    private String normalizedGameTitle(String gameTitle) {
+        return gameTitle.toLowerCase().replace(" ", "_" ) ;
     }
 }
